b829fb3b043f8abd1dc0ecd80fc5808cfeb2d4e7
madhav-relish <madhavsingh.relish@gmail.com>
2024-06-05 09:28:37 +0530

    mongo minus jwt submission

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,58 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    Admin.create({
+        username: username,
+        password: password
+    })
+    .then(function(value){
+        if(value){
+            res.json({msg: "Admin created successfully"})
+        }else{
+            res.json({msg: "Could not create Admin"})
+        }
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const title = req.body.title
+    const description = req.body.description
+    const imageLink = req.body.imageLink
+    const price = req.body.price
+
+    Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+    .then(function(value){
+        if(value){
+            res.json({msg: "Course created successfully", courseId: value._id})
+        }else{
+            res.json({msg: "Could not create Course"})
+        }
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({
+      
+    }).then(function(response){
+        res.json({
+            courses: response
+        })
+    })
 });
 
 module.exports = router;
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,67 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { Course, User } = require("../db");
 
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username = req.body.username
+    const password = req.body.password
+
+    User.create({
+        username,
+        password
+    })
+    .then(function(response){
+        if(response){
+            res.json({
+                msg: "User Creates Successfully!"
+            })
+        }else{
+            res.status(403).json({msg: "Unable to create User!"})
+        }
+    })
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+    Course.find({})
+    .then(function(response){
+        res.json({courses: response})
+    })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+    User.updateMany({
+        username: username
+    },{
+        "$push" : {
+            purchasedCourses  : courseId
+        }
+    })
+    .then(function(response){
+        res.json({msg: "Done!"})
+    })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async(req, res) => {
     // Implement fetching purchased courses logic
+ const user = await User.findOne({
+        username: req.headers.username
+    })
+    // console.log(user.purchasedCourses)
+    const courses = await Course.find({
+        _id:{
+            "$in": user.purchasedCourses
+        }
+    })
+    res.json({
+        courses
+    })
 });
 
 module.exports = router
